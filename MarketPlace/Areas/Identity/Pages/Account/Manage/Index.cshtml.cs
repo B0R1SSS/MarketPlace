159b3b466d832d51cd6e7e38062882339e5d6adc
Boris Savov <borko.savov@azet.sk>
2023-04-26 17:21:48 +0200

    added user profile settings

---
+++ MarketPlace/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using MarketPlace.Data;
+using MarketPlace.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MarketPlace.Areas.Identity.Pages.Account.Manage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly UserManager<RegisteredUser> _userManager;
+        private readonly SignInManager<RegisteredUser> _signInManager;
+        private readonly MarketPlaceDbContext _dbContext;
+
+        public IndexModel(
+            UserManager<RegisteredUser> userManager,
+            SignInManager<RegisteredUser> signInManager,
+            MarketPlaceDbContext dbContext)
+        {
+            _dbContext= dbContext;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public RegisteredUser CurrentUser { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            CurrentUser = user;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(float amount)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            user.Balance += amount;
+            await _dbContext.SaveChangesAsync();
+            return RedirectToPage();
+        }
+    }
+}
